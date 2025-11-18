5af6fc96a1c5d1b153e727e332f6dc808b6ffd0c
jneilliii <jneilliii+github@gmail.com>
2025-11-17 21:16:38 -0500

    adjustment to printing file name detection

--- octoprint_bambu_connector/connector.py
+++ octoprint_bambu_connector/connector.py
@@ -652,7 +652,9 @@ class ConnectedBambuPrinter(
         if self.state not in OPERATIONAL_STATES:
             return
 
-        if printer.subtask_name:
+        if printer.current_3mf_file:
+            current_path = printer.current_3mf_file
+        elif printer.subtask_name:
             current_path = printer.subtask_name
         elif printer.gcode_file:
             current_path = printer.gcode_file
